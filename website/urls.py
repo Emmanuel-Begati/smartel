61000ab25039e5b3b0ee657ccd3ee1bf20fb1a08
Emmanue-Begati <e.yakubu@alustudent.com>
2025-01-31 19:47:46 +0200

    making progress

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    path('', views.index, name='index'),
+    path('home/', views.index, name='index'),
+    path('base/', views.base, name='base'),
+    # path('about/', views.about, name='about'),
+    # path('services/', views.services, name='services'),
+    # path('contact/', views.contact, name='contact'),
+]
