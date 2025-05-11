a90f6e32adf94da7e1e9b8aea1b37e4b4f450c5e
Emmanuel-Begati <begati16@gmail.com>
2025-05-11 19:21:03 -0400

    done with some work

--- smartel/settings.py
+++ smartel/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "False"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sentraimpact.org", "*"]
 
 
 # Application definition
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = '/home/sentmjul/public_html/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
