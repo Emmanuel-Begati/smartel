235c5edd6d69a63f6cadb6f5d22f1f7286dfcd4a
Emmanuel-Begati <begati16@gmail.com>
2025-05-12 04:58:44 +0200

    changed

--- smartel/settings.py
+++ smartel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "False"
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sentraimpact.org", "*"]
 
