040cdf063e8bd082c1eee151eb266e21ccd72d31
Emmanuel-Begati <begati16@gmail.com>
2025-05-13 09:44:22 +0200

    done

--- smartel/settings.py
+++ smartel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "False"
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sentraimpact.org", "*"]
 
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "/home/sentmjul/public_html/static"
 STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, "static"),  # Uncomment if you create this folder
 ]
